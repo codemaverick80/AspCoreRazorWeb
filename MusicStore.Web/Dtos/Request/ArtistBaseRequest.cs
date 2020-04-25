9bd0e66c65ca1f61ea2f0de0483d22bfd1944258
hpscodemaverick <toli@HarishsMacBookPro.hsd1.ca.comcast.net>
2020-04-25 15:17:13 -0700

    artist baserequest and artist create request class added

---
+++ MusicStore.Web/Dtos/Request/ArtistBaseRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.Web.Dtos.Request
+{
+    public class ArtistBaseRequest
+    {
+
+        public string Id { get; set; }
+        [Required(ErrorMessage ="Artist Name required")]
+        public virtual string ArtistName { get; set; }
+        public virtual string YearActive { get; set; }
+        public virtual string Biography { get; set; }
+        public virtual string ThumbnailTag { get; set; }
+        public virtual string SmallThumbnail { get; set; }
+        public virtual string LargeThumbnail { get; set; }
+     
+        
+    }
+}
---
+++ MusicStore.Web/Dtos/Request/Create/ArtistCreateRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace MusicStore.Web.Dtos.Request.Create
+{
+    public class ArtistCreateRequest : ArtistBaseRequest
+    {
+        
+    }
+}
